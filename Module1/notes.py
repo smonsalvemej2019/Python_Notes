cffb3e6e5e01a96941f2ef62224391c2a06191cb
Santiago Monsalve <71195627+smonsalvemej2019@users.noreply.github.com>
2021-05-20 20:28:44 -0400

    first commit

---
+++ Module1/notes.py
@@ -0,0 +1,17 @@
+#Syntaxes
+
+#None...Python has not syntax. Well it does, but remember that you do not have to 
+#define your data type
+
+#Basic datatypes
+
+num = 1 #this is an integer
+fnumber = 2.8 #this is a decimal
+letter = 'a' #this is a character 
+strng = "Hello Painful world" #this is a string
+
+#I/O. 
+#like any other language there is basic i/o 
+
+myInput = input("Enter a number of your choice: ")
+print('This is my output:', myInput)
