ffb2086e283b6e3ee483a95868209f222897d505
Santiago Monsalve <71195627+smonsalvemej2019@users.noreply.github.com>
2021-05-20 20:54:59 -0400

    readme.md

--- Module1/notes.py
+++ Module1/notes.py
@@ -1,3 +1,6 @@
+#import
+import math #import keyword used to import libraries
+
 #Syntaxes
 
 #None...Python has not syntax. Well it does, but remember that you do not have to 
@@ -14,4 +17,23 @@ strng = "Hello Painful world" #this is a string
 #like any other language there is basic i/o 
 
 myInput = input("Enter a number of your choice: ")
+int(myInput) ##when we get input it is in a string format, int will change it to an integer
 print('This is my output:', myInput)
+
+#we can also import libraries like any other language 
+#python comes with a lot of libraries out of the box
+#libraries out of the box
+#(check the the import section at the top)
+#the math library is a useful standard library
+
+#example program: Circumference program 
+
+rad_in = input("Enter the input of your circle: ")
+int(rad_in)
+
+circum = 2 * math.pi * rad_in
+area = math.pi * (rad_in ** 2)#the ** represents power
+
+print("The area of your circle is: ", area, ", and circumference is: ", circum)
+
+#the math library has many many useful methods
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Python notes repo
+This repo is used to learn the python programing language.
+Most notes are in the python files themselves, but some important screenshots will be added to the 
+readme.
+
+## Some reserved words and operators in python
+![reserved Words](/Screenshots/resWords.png)
