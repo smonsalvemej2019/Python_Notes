d40385c4dc2f40e6e1a2eb98435fa537f6fe688f
Santiago Monsalve <71195627+smonsalvemej2019@users.noreply.github.com>
2021-06-16 19:48:20 -0400

    quickfix

---
+++ Module7/mod_7_notes2.py
@@ -0,0 +1,26 @@
+#we will be using pylab for module 7 homework, I thought it would be a good idea to go ove it
+import pylab as plt
+
+#mathplot library is the whole library pylab is mathplot + numpy
+
+#lets plot some numbers
+x = [1, 2, 3, 4]
+y = [1, 4, 9, 16]
+
+plt.plot(x,y, 'r-') #plots x vs y with a red line
+plt.ylabel('y') #labels y
+plt.xlabel('x') #lables x
+plt.savefig('my_chart.png')
+plt.show() # shows tplot
+#we can save the chart by doing
+
+
+#linetypes include 
+# - , –, -., , . , , , o , ^ , v , < , > ,
+#  s , + , x , D , d , 1 , 2 , 3 , 4 , h , H , p , | , _
+
+#colors include
+#b, g, r, c, m, y, k, w
+
+
+#there is more to pylab so please see the mathplot documentation
