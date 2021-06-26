33271008b17ca62f2249906ddab31daff451aaa3
Santiago Monsalve <71195627+smonsalvemej2019@users.noreply.github.com>
2021-06-26 12:38:05 -0400

    notes

--- Module8/mod_8_notes.py
+++ Module8/mod_8_notes.py
@@ -17,6 +17,44 @@ print(round(square_root(),2))
 #REMEMBER!!! Params are the var listed inside the parenthesis during func def
 #arguments are the values passed when the 
 
+#is python parameters passed by reference or by value?
+#Neither, python uses a mechanism called pass-by-object.
+#we can simply think of pass-by-object as giving things(a function a datatype) a name (var name)
+#in python we bind a name to an object!!!!
+#immutable objects like strings can be thought of as pass by value and mutable objects like lists are mustable
+
+#an example 
+#lets define a immutable variable
+my_string = "\nThis is my string outside the function\n"
+
+#and define a function that will change the immutable
+def mutate_strng(my_string):
+    my_string = "\nIs something different now inside the function\n"
+    print(my_string)
+
+#passing the string to the function will change the value ONLY INSIDE the function scope and not in the global
+mutate_strng(my_string)
+print(my_string)
+
+#lets define mutable variable
+my_list=['Hello','this is my','mutbale','variable']
+
+#and define a function that will change our mutable
+def mutate_list(my_list):
+    my_list[2],my_list[1] = 'mutated', 'this is now my'
+
+#notice that the variable changed in the global scope
+print(f"\n{my_list}")
+mutate_list(my_list)
+print(f"\n{my_list}")
+
+#scopes are are also very important yet very simple in python, we have four types:
+#LOCAL: Var x is inside the innermost function g()
+#ENCLOSING: if there is two nested functions f() and g() and if var x is not in the local scope of inner function g()
+#then the interpreter looks for x in the enclosing function f()
+#GLOBAL: If x is not inside any function f() or g() then it must me somwhere in the global scope (defined in the main program)
+#BUILT-IN: If x is not in the global scope then it must be a python built-in object
+
 #An important method in python is the dir()
 #while not used in actual used during implementation, dir can provide 
 #valuable information on methods
@@ -34,4 +72,5 @@ print("\n", mt.__doc__)#this is the documentation method
 print("\n",square_root().__doc__)#the documentation of the function made above
 
 #we will learn more about __special methods__ next module
+
 
